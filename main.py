1be7708275fc8364fadaa0062da3a0ae27e14e6d
RafaelPeretta <perettaineas26@gmail.com>
2025-11-01 01:49:02 -0300

    feat: Adiciona o endpoint GET /lancamentos para listar todos os lançamentos

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 import models 
-import schemas
+import schemas 
 from database import engine, SessionLocal
+from typing import List
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -21,16 +22,25 @@ def read_root():
     return {"message": "Porra, chefe, tá rodando e o banco tá pronto!"}
 
 
-@app.post("/lancamentos/")
+@app.post("/lancamentos/", response_model=schemas.Lancamento)
 def criar_lancamento(
-    lancamento: schemas.LancamentoCreate,
+    lancamento: schemas.LancamentoCreate, 
     db: Session = Depends(get_db)
 ):
     db_lancamento = models.Lancamento(
         descricao=lancamento.descricao,
         valor=lancamento.valor
     )
-    db.add(db_lancamento)
-    db.commit() 
-    db.refresh(db_lancamento)
+    
+    db.add(db_lancamento) 
+    db.commit()          
+    db.refresh(db_lancamento) 
+    
     return db_lancamento
+
+@app.get("/lancamentos/", response_model=List[schemas.Lancamento])
+def ler_lancamentos(db: Session = Depends(get_db)):
+    
+    lancamentos = db.query(models.Lancamento).all()
+    
+    return lancamentos
--- schemas.py
+++ schemas.py
@@ -1,5 +1,15 @@
 from pydantic import BaseModel
+from datetime import datetime 
 
 class LancamentoCreate(BaseModel):
     descricao: str
     valor: float
+
+class Lancamento(BaseModel):
+    id: int
+    descricao: str
+    valor: float
+    data_criacao: datetime
+
+    class Config:
+        from_attributes = True 
